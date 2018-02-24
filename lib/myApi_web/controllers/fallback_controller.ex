a40c3802ab8dd56a23ea081b35d92d4b525e294a
njwest <njwest9@gmail.com>
2018-02-24 13:24:59 +0800

    added virtual fields, validation, and password hash encryption to user schema; generated all the views

---
+++ lib/myApi_web/controllers/fallback_controller.ex
@@ -0,0 +1,20 @@
+defmodule MyApiWeb.FallbackController do
+  @moduledoc """
+  Translates controller action results into valid `Plug.Conn` responses.
+
+  See `Phoenix.Controller.action_fallback/1` for more details.
+  """
+  use MyApiWeb, :controller
+
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> render(MyApiWeb.ChangesetView, "error.json", changeset: changeset)
+  end
+
+  def call(conn, {:error, :not_found}) do
+    conn
+    |> put_status(:not_found)
+    |> render(MyApiWeb.ErrorView, :"404")
+  end
+end
---
+++ lib/myApi_web/views/user_view.ex
@@ -0,0 +1,18 @@
+defmodule MyApiWeb.UserView do
+  use MyApiWeb, :view
+  alias MyApiWeb.UserView
+
+  def render("index.json", %{users: users}) do
+    %{data: render_many(users, UserView, "user.json")}
+  end
+
+  def render("show.json", %{user: user}) do
+    %{data: render_one(user, UserView, "user.json")}
+  end
+
+  def render("user.json", %{user: user}) do
+    %{id: user.id,
+      email: user.email,
+      password_hash: user.password_hash}
+  end
+end
